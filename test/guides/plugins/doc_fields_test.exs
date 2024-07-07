df4f8c15f30dd9afc3f1e1629b7fdf9bcd3be2ea
Phil Chen <06fahchen@gmail.com>
2024-07-07 19:39:01 +0800

    docs: add doc_fields guide (#11)

---
+++ test/guides/plugins/doc_fields_test.exs
@@ -0,0 +1,39 @@
+defmodule Guides.Plugins.DocFieldsTest do
+  use TypedStructor.TestCase
+
+  @tag :tmp_dir
+  test "works", ctx do
+    doc =
+      with_tmpmodule TestModule, ctx do
+        unquote(
+          "doc_fields.md"
+          |> TypedStructor.GuideCase.extract_code()
+          |> Code.string_to_quoted!()
+        )
+      after
+        fetch_doc!(TestModule.User, {:type, :t, 1})
+      end
+
+    expected = """
+    This is a user struct.
+
+
+    ## Parameters
+
+    | Name | Description |
+    |------|-------------|
+    |`:age` | The age parameter.|
+
+
+    ## Fields
+
+    | Name | Type | Description |
+    |------|------|-------------|
+    |`:name` | `String.t() | nil` | The name of the user.|
+    |`:age` | `age | nil` | The age of the user.|
+
+    """
+
+    assert expected === doc
+  end
+end
